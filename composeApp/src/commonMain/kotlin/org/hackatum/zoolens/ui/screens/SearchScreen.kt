2116bdf7b897b9ae416f3ea10ed14f66f26fc32d
Sven Fydrich <sven.fydi@gmail.com>
2025-11-22 14:48:41 +0100

    feature: Add Animal route for AnimalScreen activity

--- composeApp/src/commonMain/kotlin/org/hackatum/zoolens/ui/screens/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/org/hackatum/zoolens/ui/screens/SearchScreen.kt
@@ -1,17 +1,29 @@
 package org.hackatum.zoolens.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.hackatum.zoolens.i18n.LocalStrings
 
 @Composable
-fun SearchScreen() {
+fun SearchScreen(onOpenAnimal: () -> Unit = {}) {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(LocalStrings.current.search, style = MaterialTheme.typography.headlineMedium)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+        ) {
+            Text(LocalStrings.current.search, style = MaterialTheme.typography.headlineMedium)
+            Button(onClick = onOpenAnimal) {
+                Text("Open Animal")
+            }
+        }
     }
 }
