06ac2b6c673f6a8dea90e499329b535d6ef84fc6
felixenke <felix.enke@tum.de>
2025-11-22 18:15:14 +0100

    SearchScreen update is ready (pull from server missing)

--- composeApp/src/commonMain/kotlin/org/hackatum/zoolens/ui/screens/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/org/hackatum/zoolens/ui/screens/SearchScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
